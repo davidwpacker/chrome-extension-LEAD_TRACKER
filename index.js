4bac8ae6e73c8e0abb7bd8e464af00157a06d70b
davidwpacker <davidwgpacker@gmail.com>
2021-06-01 19:21:12 +1000

    delete button functionality added

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el");
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+deleteBtn.addEventListener("dblclick", function() {
+    console.log("double clicked!")
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    inputEl.value = "" // this clears the input field after clicking submit button
-    renderLeads();
-})
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
  
-function renderLeads() {
-let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>" }
-    // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-    listItems += 
-    `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'>
-            ${myLeads[i]} </a>
-        </li>
-    `
-    // template string/literals
+    console.log( localStorage.getItem("myLeads") ) // verfification
+})
 
-ulEl.innerHTML = listItems
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
 }
 
